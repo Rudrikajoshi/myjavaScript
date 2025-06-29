d58dfabe1d2c17e9399ad92b88f20752cebed790
Rudrikajoshi <142895294+Rudrikajoshi@users.noreply.github.com>
2025-06-29 09:06:08 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //this helps in treating all JS code as newer version
+//alert(4 + 4) this will not work here because we are using node.js (only works on browser)
+ 
+
+let name ="Rudrika" //string data type
+let age = 18        //integer datatype
+let isLoggedIn = true // boolean datatype
+//null datatype => standalone value
+//undefined =>
+//symbol => uniqueness
+
+console.log(typeof "Aman") //typeof helps you give the name of datatype
